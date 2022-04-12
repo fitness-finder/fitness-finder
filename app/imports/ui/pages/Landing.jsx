ae5fac7873021f3cee892efe4dde054d9c67e28d
bhavanikay <98071288+bhavanikay@users.noreply.github.com>
2022-04-12 09:41:15 -1000

    landing_fix

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Grid, Header, Reveal, Image } from 'semantic-ui-react';
 
+const font = {
+  fontFamily: 'Montserrat',
+};
 const styles1 = {
   textAlign: 'center',
 };
@@ -16,10 +19,10 @@ class Landing extends React.Component {
         <div className="header-background">
           <Grid verticalAlign='middle' textAlign='center' container>
             <Grid.Column>
-              <Header style={{ paddingTop: '100px', color: 'white', fontSize: '45pt' }} as='h1'>
+              <Header style={{ paddingTop: '110px', color: 'white', fontSize: '45pt', fontFamily: 'Montserrat' }} as='h1'>
                 Fitness Finder
               </Header>
-              <Header style={{ paddingBottom: '80px', color: 'white', fontSize: '25pt' }} as='h3'>
+              <Header style={{ paddingBottom: '70px', color: 'white', fontSize: '25pt', fontFamily: 'Montserrat' }} as='h3'>
                 Your fitness journey starts here!
               </Header>
             </Grid.Column>
@@ -28,20 +31,20 @@ class Landing extends React.Component {
         <div className="middle-background">
           <Grid container centered stackable columns={3}>
             <Grid.Column textAlign='center'>
-              <Header as="h1" inverted>Stay Healthy</Header>
+              <Header style={font} as="h2" inverted>Stay Healthy</Header>
               <div style={styles1}>
                 <Reveal animated="fade">
                   <Reveal.Content visible style={styles2}>
                     <Image
                       src="https://static01.nyt.com/images/2022/01/03/realestate/04fix1/oakImage-1641226833782-mediumSquareAt3X.jpg"
-                      size="medium"
+                      size="large"
                       centered
                     />
                   </Reveal.Content>
                   <Reveal.Content hidden style={styles2}>
                     <Image
-                      src="https://react.semantic-ui.com/images/avatar/large/ade.jpg"
-                      size="medium"
+                      src="images/fact1.png"
+                      size="large"
                       centered
                     />
                   </Reveal.Content>
@@ -50,20 +53,20 @@ class Landing extends React.Component {
             </Grid.Column>
 
             <Grid.Column textAlign='center'>
-              <Header as="h1" inverted>Make Friends</Header>
+              <Header style={font} as="h2" inverted>Make Friends</Header>
               <div style={styles1}>
                 <Reveal animated="fade">
                   <Reveal.Content visible style={styles2}>
                     <Image
                       src="https://static01.nyt.com/images/2021/06/07/us/07xp-polo/07xp-polo-mediumSquareAt3X.jpg"
-                      size="medium"
+                      size="large"
                       centered
                     />
                   </Reveal.Content>
                   <Reveal.Content hidden style={styles2}>
                     <Image
-                      src="https://react.semantic-ui.com/images/avatar/large/ade.jpg"
-                      size="medium"
+                      src="images/fact2.png"
+                      size="large"
                       centered
                     />
                   </Reveal.Content>
@@ -72,21 +75,21 @@ class Landing extends React.Component {
             </Grid.Column>
 
             <Grid.Column textAlign='center'>
-              <Header as="h1" inverted>Explore Interests</Header>
+              <Header style={font} as="h2" inverted>Explore Interests</Header>
               <div style={styles1}>
                 <Reveal animated="fade">
                   <Reveal.Content visible style={styles2}>
                     <Image
                       src="https://www.adventure-journal.com/wp-content/uploads/2014/03/MorganMaassen_Instagram_2.jpg"
-                      size="medium"
+                      size="large"
                       centered
                     />
                   </Reveal.Content>
                   <Reveal.Content hidden style={styles2}>
                     <Image
-                      src="images/pic.png"
+                      src="images/fact3.png"
                       alt="Pls help"
-                      size="medium"
+                      size="large"
                       centered
                     />
                   </Reveal.Content>
