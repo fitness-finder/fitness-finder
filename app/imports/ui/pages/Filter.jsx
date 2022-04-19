6fd576db352102ecdafeaa9f69a1698922455dd6
bhavanikay <98071288+bhavanikay@users.noreply.github.com>
2022-04-19 10:16:27 -1000

    Update Filter.jsx

--- app/imports/ui/pages/Filter.jsx
+++ app/imports/ui/pages/Filter.jsx
@@ -32,22 +32,27 @@ function getProfileData(email) {
 const MakeCard = (props) => (
   <Card>
     <Card.Content>
-      <Image floated='right' size='mini' src={props.profile.picture} />
+      <Image floated='right' size='mini' src={props.profile.picture}/>
       <Card.Header>{props.profile.firstName} {props.profile.lastName}</Card.Header>
       <Card.Meta>
-        <span className='date'>{props.profile.title}</span>
+        <span className='date'>{props.profile.year}</span>
       </Card.Meta>
       <Card.Description>
         {props.profile.bio}
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
+      <Header as='h5'>Interests</Header>
       {_.map(props.profile.interests,
         (interest, index) => <Label key={index} size='tiny' color='teal'>{interest}</Label>)}
     </Card.Content>
     <Card.Content extra>
-      <Header as='h5'>Projects</Header>
-      {_.map(props.profile.projects, (project, index) => <Image key={index} size='mini' src={project}/>)}
+      <Header as='h5'>Sessions</Header>
+      {_.map(props.profile.sessions, (session, index) => <Label key={index} size='tiny' color='teal'>{session}</Label>)}
+    </Card.Content>
+    <Card.Content extra>
+      <Header as='h5'>Joined Sessions</Header>
+      {_.map(props.profile.participation, (session, index) => <Label key={index} size='tiny' color='teal'>{session}</Label>)}
     </Card.Content>
   </Card>
 );
